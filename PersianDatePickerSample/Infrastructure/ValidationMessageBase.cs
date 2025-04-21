ec5c6af696b1e0f9d0a4539eaaec29c001522dc8
sarikhani-mar <sarikhani-mar@ICTB01F07C5102>
2025-04-21 08:33:08 +0330

    Version 1.0.3

---
+++ PersianDatePickerSample/Infrastructure/ValidationMessageBase.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System.Linq.Expressions;
+
+namespace TMPersianDatePicker.Infrastructure;
+
+public class ValidationMessageBase<TValue> : ComponentBase, IDisposable
+{
+    [CascadingParameter]
+    public EditContext EditContext { get; set; }
+
+    [Parameter]
+    public Expression<Func<TValue>> For { get; set; }
+
+    [Parameter]
+    public EventCallback<bool> OnValidationChanged { get; set; }
+
+    private FieldIdentifier _fieldIdentifier;
+
+    protected override void OnInitialized()
+    {
+        _fieldIdentifier = FieldIdentifier.Create(For);
+        EditContext.OnValidationStateChanged += HandleValidationStateChanged;
+    }
+
+    private void HandleValidationStateChanged(object o, ValidationStateChangedEventArgs args)
+    {
+        var status = EditContext.IsValid(_fieldIdentifier);
+        OnValidationChanged.InvokeAsync(status);
+        StateHasChanged();
+    }
+
+    public void Dispose()
+    {
+        EditContext.OnValidationStateChanged -= HandleValidationStateChanged;
+    }
+
+    public void Immediate() => EditContext.NotifyFieldChanged(_fieldIdentifier);
+}
